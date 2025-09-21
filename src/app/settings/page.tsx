f0fb16d1cab41ada1c6556a8c5221fa7ff78fa0a
Poojitha Lanka <lakshmipoojithalanka44@gmail.com>
2025-09-21 10:29:47 +0000

    side tool var button not working well

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,19 @@
+import MainLayout from '@/components/main-layout';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+export default function SettingsPage() {
+  return (
+    <MainLayout>
+      <div className="p-4 md:p-8">
+        <Card>
+          <CardHeader>
+            <CardTitle>Settings</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>This is the settings page. App settings will be available here.</p>
+          </CardContent>
+        </Card>
+      </div>
+    </MainLayout>
+  );
+}
